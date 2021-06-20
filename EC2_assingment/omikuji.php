de3fd486bcb12682478ec8ed9fb81fa38fa48f84
YoshitakaChiba <77787547+YoshitakaChiba@users.noreply.github.com>
2021-06-20 20:44:52 +0900

    test2

--- EC2_assingment/omikuji.php
+++ EC2_assingment/omikuji.php
@@ -15,7 +15,7 @@
     <table border="1" id="targetTable">
       <thead>
         <tr>
-          <td>NNNo.</td>
+          <td>No.</td>
           <td>おみくじ結果</td>
           <td>時間</td>
         </tr>
