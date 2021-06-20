34214a85faad352b73728ec1e86eb6b7af14798c
YoshitakaChiba <77787547+YoshitakaChiba@users.noreply.github.com>
2021-06-20 20:39:03 +0900

    DB-change-rds

--- EC2_assingment/php/omi_insert.php
+++ EC2_assingment/php/omi_insert.php
@@ -1,6 +1,6 @@
 <?php
 
-$s=new PDO("mysql:host=localhost;dbname=db1","root", "Yo1_brz0");
+$s=new PDO("mysql:host=database-1.c64af0vgcsyg.ap-northeast-1.rds.amazonaws.com;dbname=DB1_PM","admin", "Elatico10");
 $result_d = $_POST['result'];//omiresultのカラムを入れる
 
 $s->query("INSERT INTO tb_omi (omiresult, time) VALUES ('$result_d', now())");
